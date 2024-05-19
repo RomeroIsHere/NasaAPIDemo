3d6fbac6e57594c0dc1870233a8d5be58e2c81f9
RomeroIsHere <guillermoromecu@gmail.com>
2024-05-18 20:40:37 -0600

    Added ImageData Retriever for ITEXT

--- src/main/java/com/example/nasaapidemo/apicontroller/ImageRetriever.java
+++ src/main/java/com/example/nasaapidemo/apicontroller/ImageRetriever.java
@@ -1,13 +1,16 @@
 package com.example.nasaapidemo.apicontroller;
 
+import com.itextpdf.io.image.ImageData;
 import javafx.scene.image.Image;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.http.HttpRequest;
+import com.itextpdf.io.image.ImageDataFactory;
 
 public class ImageRetriever extends AbstractHTTPConnect{
     public Image getFromInputStream(InputStream stream){
@@ -18,6 +21,9 @@ public class ImageRetriever extends AbstractHTTPConnect{
         URL url = new URI(imageUrl).toURL();
         InputStream is = url.openStream();
         return getFromInputStream(is);
+    }
+    public ImageData getDataFromURL(String imageUrl) throws MalformedURLException {
+        return ImageDataFactory.create(imageUrl);
     }
 
 }
